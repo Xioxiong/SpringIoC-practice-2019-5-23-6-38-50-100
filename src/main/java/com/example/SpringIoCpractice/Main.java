5aa47e6f7a2776519a5e21748e97110821003508
xiongwei <2894001817@qq.com>
2019-07-25 20:48:03 +0800

    修改了main文件和hell.xml文件

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,18 @@
+package com.example.SpringIoCpractice;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
 //TODO
+public class Main{
+
+
+    public static void main(String[] args){
+        ApplicationContext applicationContext = new FileSystemXmlApplicationContext("hello.xml");
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        helloWorld.getHello();
+    }
+}
